4272dc62df83bbb3830fe1c19e5eed54202fca9d
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-28 18:19:46 +0100

    add class loginInfo

---
+++ src/main/java/com/example/InsuranceApplication/LoginInfo.java
@@ -0,0 +1,27 @@
+package com.example.InsuranceApplication;
+
+public class LoginInfo {
+    private String nickName;
+    private String password;
+
+    public LoginInfo(String nickName, String password) {
+        this.nickName = nickName;
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
