1c21220f4c3f3cdce0f91ac97cb0401d5db82474
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-30 14:35:58 +0100

    create @entity and add anotations

--- src/main/java/com/example/InsuranceApplication/client/Client.java
+++ src/main/java/com/example/InsuranceApplication/client/Client.java
@@ -1,18 +1,35 @@
 package com.example.InsuranceApplication.client;
 
 import com.example.InsuranceApplication.Insurance;
+import jakarta.persistence.*;
 
 import java.util.Date;
-
+@Entity
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "personalData")
     private PersonalData personalData;
+    @Column(name = "loginInfo")
     private LoginInfo loginInfo;
+    @Column(name = "insuranceInfo")
     private Insurance insuranceInfo;
 
-    public Client(PersonalData personalData, LoginInfo loginInfo, Insurance insuranceInfo) {
+    public Client(Long id, PersonalData personalData, LoginInfo loginInfo, Insurance insuranceInfo) {
+        this.id = id;
         this.personalData = personalData;
         this.loginInfo = loginInfo;
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public PersonalData getPersonalData() {
