6269dd0c5a33d7c130f5d64a9156357d9b1d2395
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-30 12:20:52 +0100

    create database connection and datasource

---
+++ src/main/java/com/example/InsuranceApplication/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package com.example.InsuranceApplication;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+
+@Configuration
+public class DatabaseConnection {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl ;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword ;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+
+    }
+
+
+//            statement = connection.createStatement();
+//
+//          statement.execute("CREATE TABLE IF NOT EXISTS meals (" +
+//             "category varchar NOT NULL," +
+//                  "meal varchar NOT NULL," +
+//                "meal_id INTEGER NOT NULL PRIMARY KEY" +
+//                  ")");
+
+
+}
