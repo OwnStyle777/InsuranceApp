2cd55a7803d7af8b46a616f67cf7d3d9c8afa753
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-30 13:02:34 +0100

    add som info to .gitignore

--- src/main/java/com/example/InsuranceApplication/DatabaseConnection.java
+++ src/main/java/com/example/InsuranceApplication/DatabaseConnection.java
@@ -31,7 +31,7 @@ public class DatabaseConnection {
         return dataSource;
 
     }
-    
+
 //            statement = connection.createStatement();
 //
 //          statement.execute("CREATE TABLE IF NOT EXISTS meals (" +
