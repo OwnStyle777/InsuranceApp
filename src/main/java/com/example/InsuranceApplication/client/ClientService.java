fa635e203d9cf095520732789c92fd7124287645
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-30 17:07:37 +0100

    add ClientServise class,where will be the logic, and add first method createClient

---
+++ src/main/java/com/example/InsuranceApplication/client/ClientService.java
@@ -0,0 +1,9 @@
+package com.example.InsuranceApplication.client;
+
+import com.example.InsuranceApplication.Insurance;
+
+public class ClientService {
+   public Client createClient (){
+       return new Client();
+   }
+}
