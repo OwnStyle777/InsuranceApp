cfb2b89f8b18b4f8cc6331bccd6ded201575ed0a
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-30 12:23:40 +0100

    add test for Db connection

--- src/main/java/com/example/InsuranceApplication/InsuranceApplication.java
+++ src/main/java/com/example/InsuranceApplication/InsuranceApplication.java
@@ -3,6 +3,8 @@ package com.example.InsuranceApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.PreparedStatement;
+
 @SpringBootApplication
 public class InsuranceApplication {
 
--- src/test/java/com/example/InsuranceApplication/ClientInsuranceAppApplicationTests.java
+++ src/test/java/com/example/InsuranceApplication/ClientInsuranceAppApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.InsuranceApplication;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class ClientInsuranceAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void testConnection() {
+		try {
+			dataSource.getConnection();
+		} catch (SQLException e) {
+
+			System.out.println("Connection to database couldn't established");
+		}
 	}
 
 }
