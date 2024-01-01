32eba1107ee897056ad6c734bd6e652dd8f42359
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-01 16:27:30 +0100

    create enum InsuranceCompanies

---
+++ src/main/java/com/example/InsuranceApplication/InsuranceCompanies.java
@@ -0,0 +1,14 @@
+package com.example.InsuranceApplication;
+
+public enum InsuranceCompanies {
+    DOVERA,
+    ALLIANZ,
+    GENERALI,
+    UNIQA,
+    UNION,
+    KOOPERATIVA,
+    KOMUNALNA,
+    CSOB,
+    WUSTENROT,
+    METLIFE
+}
