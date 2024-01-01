180322722d125977b396c2a211659cf028dcefd0
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-01 10:54:57 +0100

    add isEmailValid function to EmailValidator interface

--- src/main/java/com/example/InsuranceApplication/verification/EmailValidator.java
+++ src/main/java/com/example/InsuranceApplication/verification/EmailValidator.java
@@ -1,4 +1,13 @@
 package com.example.InsuranceApplication.verification;
 
 public interface EmailValidator {
+
+  default boolean isEmailValid (String email){
+      String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+      if(!email.matches(regex)){
+          System.out.println("Wrong format of email address.");
+          return false;
+      }
+      return true;
+  }
 }
