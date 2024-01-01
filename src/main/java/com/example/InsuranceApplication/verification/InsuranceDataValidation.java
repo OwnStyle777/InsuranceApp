48b55e3da6d0ea02bd9f7abb0e21f9cef5184697
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-01 16:30:35 +0100

    add method isValidInsuranceCompany to InsuranceDataValidation interface

--- src/main/java/com/example/InsuranceApplication/verification/InsuranceDataValidation.java
+++ src/main/java/com/example/InsuranceApplication/verification/InsuranceDataValidation.java
@@ -1,8 +1,19 @@
 package com.example.InsuranceApplication.verification;
 
+import com.example.InsuranceApplication.InsuranceCompanies;
+
 public interface InsuranceDataValidation {
     default boolean isBirthNumberValid(int birthNumber){
         return String.valueOf(birthNumber).length() == 10;
+    }
+
+    default boolean isValidInsuranceCompany(String company) {
+        for (InsuranceCompanies insuranceCompany : InsuranceCompanies.values()) {
+            if (insuranceCompany.name().equalsIgnoreCase(company)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
