7e8a144d9b16eb76b6a879021b95986e77cace14
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-06 12:15:59 +0100

    add anotation for remove default security login

--- src/main/java/com/example/InsuranceApplication/InsuranceApplication.java
+++ src/main/java/com/example/InsuranceApplication/InsuranceApplication.java
@@ -4,10 +4,11 @@ import com.example.InsuranceApplication.client.Client;
 import com.example.InsuranceApplication.client.ClientService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.sql.PreparedStatement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class InsuranceApplication {
 
 	public static void main(String[] args) {
