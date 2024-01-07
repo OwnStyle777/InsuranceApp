389b46750e6288bde0209a4cef5d0ad5c0c70545
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-07 16:58:10 +0100

    create RegistrationForm class

---
+++ src/main/java/com/example/InsuranceApplication/RegistrationForm.java
@@ -0,0 +1,76 @@
+package com.example.InsuranceApplication;
+
+public class RegistrationForm {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String birthDate;
+    private String birthNumber;
+    private String phoneNumber;
+    private String insuranceCompany;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getBirthNumber() {
+        return birthNumber;
+    }
+
+    public void setBirthNumber(String birthNumber) {
+        this.birthNumber = birthNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    public void setInsuranceCompany(String insuranceCompany) {
+        this.insuranceCompany = insuranceCompany;
+    }
+}
