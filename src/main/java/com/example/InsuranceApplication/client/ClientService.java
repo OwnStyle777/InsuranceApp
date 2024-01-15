87a3f080ee261cee48608985256041236147bfe3
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-15 11:47:32 +0100

    add createForm to ClientService

--- src/main/java/com/example/InsuranceApplication/client/ClientService.java
+++ src/main/java/com/example/InsuranceApplication/client/ClientService.java
@@ -1,16 +1,26 @@
 package com.example.InsuranceApplication.client;
 
-import com.example.InsuranceApplication.RegistrationForm;
+import com.example.InsuranceApplication.forms.RegistrationForm;
 import com.example.InsuranceApplication.insurance.Insurance;
 import com.example.InsuranceApplication.insurance.InsuranceDataGeneration;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Service
 public class ClientService {
+
+    public  RegistrationForm createForm (String firstName, String lastName, String email, String password, String birthDate, String birthNumber,String phoneNumber, String insuranceCompany){
+
+        RegistrationForm form = new RegistrationForm();
+        form.setFirstName(firstName);
+        form.setLastName(lastName);
+        form.setEmail(email);
+        form.setPassword(password);
+        form.setBirthDate(birthDate);
+        form.setPhoneNumber(phoneNumber);
+        form.setBirthNumber(birthNumber);
+        form.setInsuranceCompany(insuranceCompany);
+        return form;
+    }
    public Client createClient (RegistrationForm registrationForm){
        Client client = new Client();
        client.setPersonalData(createPersonalData(registrationForm));
@@ -26,7 +36,7 @@ public class ClientService {
        PersonalData personalData = new PersonalData();
        personalData.setFirstName(registrationForm.getFirstName());
        personalData.setSecondName(registrationForm.getLastName());
-       personalData.setBirthDate(new Date(registrationForm.getBirthDate()));
+       personalData.setBirthDate(registrationForm.getBirthDate());
        personalData.setNumber(registrationForm.getPhoneNumber());
        return personalData;
    }
@@ -45,7 +55,7 @@ public class ClientService {
        insuranceInfo.setIdentificationNumberOfInsured(dataGeneration.generateIdentificationNumber());
        insuranceInfo.setInsuranceNumber(dataGeneration.generateInsuranceNumber(registrationForm.getInsuranceCompany()));
        insuranceInfo.setNameOfInsuranceCompany(registrationForm.getInsuranceCompany());
-       insuranceInfo.setBirthNumber(Integer.parseInt(registrationForm.getBirthNumber()));
+       insuranceInfo.setBirthNumber(registrationForm.getBirthNumber());
 
        return insuranceInfo;
 
