1de376513ed806832bb2627dce6308266e85fc55
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-15 11:49:08 +0100

    change Post methods from RequestBody to RequestParam

--- src/main/java/com/example/InsuranceApplication/client/ClientController.java
+++ src/main/java/com/example/InsuranceApplication/client/ClientController.java
@@ -1,28 +1,20 @@
 package com.example.InsuranceApplication.client;
 
 
-import com.example.InsuranceApplication.RegistrationForm;
-import com.example.InsuranceApplication.insurance.Insurance;
-import com.example.InsuranceApplication.insurance.InsuranceDataGeneration;
+import com.example.InsuranceApplication.forms.LoginForm;
+import com.example.InsuranceApplication.forms.RegistrationForm;
 import com.example.InsuranceApplication.verification.ClientValidator;
 import com.example.InsuranceApplication.verification.EmailValidator;
 import com.example.InsuranceApplication.verification.PasswordValidator;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.swing.text.html.HTML;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Date;
-
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 
 public class ClientController implements EmailValidator, PasswordValidator, ClientValidator {
     @Autowired
@@ -31,67 +23,22 @@ public class ClientController implements EmailValidator, PasswordValidator, Clie
     SessionFactory sessionFactory ;
 
 
-//    @GetMapping ("/clientInfo")
-//    public ResponseEntity<String> createClient() {
-//        Client createdClient = clientService.createClient();
-//        String clientInfo = "Client created: " + createdClient.getPersonalData().getFirstName(); // Príklad, ako by mohol vyzerať reťazec s informáciami o klientovi
-//        return ResponseEntity.ok(clientInfo);
-//    }
-    @GetMapping("/register")
-    public ResponseEntity<byte[]> showRegisterPage() throws IOException {
-        ClassPathResource htmlFile = new ClassPathResource("static/form.html");
-        byte[] html = Files.readAllBytes(Path.of(htmlFile.getURI()));
-        return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(html);
-    }
-    @GetMapping ("/hello")
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello, this is a test endpoint!");
-    }
+    @PostMapping(value = "/login", consumes = {"application/json"}, produces = {"application/json"})
+    public ResponseEntity<?> loginUser ( @RequestBody LoginForm loginForm) {
+        String email = loginForm.getEmail();
+        String password = loginForm.getPassword();
 
 
-//    @PostMapping ("/register")
-//    public ResponseEntity<?> registerUser(@RequestBody Client client){
-//        ClientDAO dao = new ClientDAO(sessionFactory);
-//        if(validateClient(client)){
-//            dao.saveClient(client);
-//            return ResponseEntity.ok().body("Registration was successful!");
-//        }
-//       return ResponseEntity.badRequest().body("Please provide all necessary information to complete registration");
-//    }
-@PostMapping("/register")
-public ResponseEntity<?> registerUser(RegistrationForm registrationForm) {
-    // Vytvorenie objektu Client a nastavenie údajov z RegistrationForm
-
-    Client client = clientService.createClient(registrationForm);
-    ClientDAO dao = new ClientDAO(sessionFactory);
-    if (validateClient(client)) {
-        if (dao.saveClient(client)) {
-            return ResponseEntity.ok().body("Registration was successful!");
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save the client.");
-        }
-    } else {
-        return ResponseEntity.badRequest().body("Please provide all necessary information to complete registration");
-    }
-}
-
-    @GetMapping("/login")
-    public ResponseEntity<byte[]> showLoginPage() throws IOException {
-        ClassPathResource htmlFile = new ClassPathResource("static/login.html");
-        byte[] html = Files.readAllBytes(Path.of(htmlFile.getURI()));
-        return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(html);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser (@RequestParam("email") String email, @RequestParam("password") String password){
         ClientDAO dao = new ClientDAO(sessionFactory);
+        System.out.println("email adress : " + email);
+        System.out.println("password : " + password);
         if (dao.isEmailInDatabase(email)) {
             Client client = dao.getClientByEmail(email);
-            if (client != null && isPasswordValid(password,client.getLoginInfo().getPassword())) {
+            if (client != null && isPasswordLengthOK(password)) {
                 RedirectView redirectView = new RedirectView();
                 redirectView.setUrl("/clientInfo");
-                // Vrátenie úspešnej odpovede
-                return ResponseEntity.ok().body(redirectView);
+
+                return ResponseEntity.ok().header("Location", "/clientInfo").build();
             } else {
 
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password must contain at least one special character, uppercase and lowercase and one digit.");
@@ -100,4 +47,41 @@ public ResponseEntity<?> registerUser(RegistrationForm registrationForm) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This email address is not registered.");
         }
     }
+
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            @RequestParam("birthDate") String birthDate,
+            @RequestParam("birthNumber") String birthNumber,
+            @RequestParam("phoneNumber") String phoneNumber,
+            @RequestParam("insurance") String insurance) {
+
+        // Create a user object based on the extracted data
+        RegistrationForm form = clientService.createForm(firstName, lastName, email, password, birthDate, birthNumber, phoneNumber, insurance);
+
+        System.out.println(form.toString());
+
+        Client client = clientService.createClient(form);
+
+        // Validate form data and perform any necessary processing
+    ClientDAO dao = new ClientDAO(sessionFactory);
+    if (validateClient(client)) {
+        if (dao.saveClient(client)) {
+
+            return ResponseEntity.ok().body("Registration was successful!");
+
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save the client.");
+        }
+    } else {
+        return ResponseEntity.badRequest().body("Please provide all necessary information to complete registration");
+    }
+}
+
 }
