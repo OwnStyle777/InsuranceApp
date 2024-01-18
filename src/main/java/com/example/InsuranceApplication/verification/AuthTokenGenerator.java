2e12cd005e888dee34c5cde314c08280ee7ddc91
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-18 20:19:04 +0100

    add two methods to AuthTokenGenerator for encoding token a getId and ExpirationDate

--- src/main/java/com/example/InsuranceApplication/verification/AuthTokenGenerator.java
+++ src/main/java/com/example/InsuranceApplication/verification/AuthTokenGenerator.java
@@ -2,11 +2,39 @@ package com.example.InsuranceApplication.verification;
 
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.Date;
 
 public class AuthTokenGenerator {
-    public static String generateAuthToken(){
-        byte[] randomBytes = new byte[32]; // Adjust the length based on your requirements
-        new SecureRandom().nextBytes(randomBytes);
-        return Base64.getEncoder().encodeToString(randomBytes);
+    public static String generateAuthToken(int userId){
+
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + 1000 * 60 * 60 * 24); // Expires in 24 hours
+
+        // Generate the token
+        String token = userId + "." + expirationDate.getTime();
+
+        return Base64.getEncoder().encodeToString(token.getBytes());
+    }
+
+    public static int getUserId(String token) {
+        // Decode the token
+        byte[] bytes = Base64.getDecoder().decode(token);
+
+        // Split the token
+        String[] parts = new String(bytes).split("\\.");
+
+        // Return the user ID
+        return Integer.parseInt(parts[0]);
+    }
+
+    public static Date getExpirationDate(String token) {
+        // Decode the token
+        byte[] bytes = Base64.getDecoder().decode(token);
+
+        // Split the token
+        String[] parts = new String(bytes).split("\\.");
+
+        // Return the expiration date
+        return new Date(Long.parseLong(parts[1]));
     }
 }
