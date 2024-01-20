e88b86a66efc57895fb9ca102811407b6f63809c
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-20 20:42:37 +0100

    add to js function , when is toggled item active change color

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -32,5 +32,45 @@
            sidebarElement.classList.toggle('collapsed');
        }
    }
+
+// Počkáme, až se načte celý dokument
+document.addEventListener('DOMContentLoaded', function () {
+  // Získáme referenci na navigační menu ve sidebaru
+  var sidebarNav = document.getElementById('sidebarNav');
+
+  // Přidáme posluchač události pro kliknutí na navigační odkazy
+  sidebarNav.addEventListener('click', function (event) {
+    // Zabráníme výchozí akci kliknutí (např. skoku na odkaz)
+    event.preventDefault();
+
+    // Získáme odkaz, na který bylo kliknuto
+    var clickedItem = event.target.closest('.nav-link');
+
+    // Pokud byl kliknutý nějaký odkaz
+    if (clickedItem) {
+
+      var allNavLinks = sidebarNav.getElementsByClassName('nav-link');
+      if (!clickedItem.classList.contains('active')) {
+             for (var i = 0; i < allNavLinks.length; i++) {
+               allNavLinks[i].classList.remove('active');
+             }
+
+             clickedItem.classList.add('active');
+
+             var contentId = clickedItem.getAttribute('data-content');
+             var selectedContent = document.getElementById(contentId);
+
+             var allContents = document.getElementsByClassName('content');
+             for (var k = 0; k < allContents.length; k++) {
+               allContents[k].style.display = 'none';
+             }
+
+             if (selectedContent) {
+               selectedContent.style.display = 'block';
+             }
+           }
+    }
+  });
+});
 })();
 
