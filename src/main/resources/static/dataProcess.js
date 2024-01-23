9e63a7f4afff77939b372d1426d9acc3249f4e62
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-23 15:00:29 +0100

    add som properties to method displayClientInfo

--- src/main/resources/static/dataProcess.js
+++ src/main/resources/static/dataProcess.js
@@ -1,26 +1,15 @@
-function processLoginData(data) {
-    // Tu môžete spracovať údaje, ktoré ste získali z backendu
-    const authToken = data.authToken;
-    const client = data.client;
-    console.log(client);
-       if (Array.isArray(client) && client.length > 0) {
-                // Předpokládáme, že první prvek pole obsahuje potřebná data
-                displayClientInfo(client[0]);
-            } else {
-                // Pokud není pole k dispozici, můžete provést další akce nebo upravit kód podle potřeby
-                console.error("Chyba: Pole s daty není dostupné nebo je prázdné.");
-            }
-
-
-    // Tu môžete robiť, čo potrebujete s týmito údajmi, napríklad zobraziť informácie o klientovi
-    displayClientInfo(client);
-
-    // Taktiež môžete pracovať s autentizačným tokenom, ak je to potrebné
-    // Napríklad, uložiť ho do sessionStorage alebo localStorage
-    // sessionStorage.setItem('authToken', authToken);
-}
 
 function displayClientInfo(client) {
-    // Tu môžete zobraziť informácie o klientovi na stránke
-    // Napríklad, aktualizovať obsah elementov na stránke podľa informácií z objektu klient
-}
+ // Získanie referencie na elementy v HTML
+//
+  const userFirstName = document.getElementById('userFirstName');
+  const personalData = document.getElementById('personalDataContent');
+  const insuranceInformation = document.getElementById('insuranceDataContent');
+  // Vymazanie predchádzajúcich údajov (ak existujú)
+
+
+ userFirstName.textContent = "  " + client.personalData.firstName;
+ personalData.textContent = client.personalData.firstName + " " + client.personalData.secondName;
+insuranceInformation.innerHTML =   "<br>" + "<b>ID of Insured:</b> " + "<br>" + client.insuranceInfo.identificationNumberOfInsured + "<br>" +"<br>" +
+                                  "<b>Insurance Company</b>: "+ "<br>" + client.insuranceInfo.nameOfInsuranceCompany + "<br>" +"<br>" +
+                                  "<b>Birth number:</b> "+ "<br>" + client.insuranceInfo.birthNumber;}
