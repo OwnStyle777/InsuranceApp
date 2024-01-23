18e24069c21fc2f7e58b929c70d66a67fd546da5
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-23 10:53:55 +0100

    make correction on methods and add new method validateToken

--- src/main/java/com/example/InsuranceApplication/verification/AuthTokenGenerator.java
+++ src/main/java/com/example/InsuranceApplication/verification/AuthTokenGenerator.java
@@ -1,11 +1,15 @@
 package com.example.InsuranceApplication.verification;
 
+import com.example.InsuranceApplication.client.Client;
+import com.example.InsuranceApplication.client.ClientDAO;
+import org.hibernate.SessionFactory;
+
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Date;
 
 public class AuthTokenGenerator {
-    public static String generateAuthToken(int userId){
+    public static String generateAuthToken(long userId){
 
         Date now = new Date();
         Date expirationDate = new Date(now.getTime() + 1000 * 60 * 60 * 24); // Expires in 24 hours
@@ -16,7 +20,7 @@ public class AuthTokenGenerator {
         return Base64.getEncoder().encodeToString(token.getBytes());
     }
 
-    public static int getUserId(String token) {
+    public static long getUserId(String token) {
         // Decode the token
         byte[] bytes = Base64.getDecoder().decode(token);
 
@@ -24,7 +28,7 @@ public class AuthTokenGenerator {
         String[] parts = new String(bytes).split("\\.");
 
         // Return the user ID
-        return Integer.parseInt(parts[0]);
+        return Long.parseLong(parts[0]);
     }
 
     public static Date getExpirationDate(String token) {
@@ -36,5 +40,27 @@ public class AuthTokenGenerator {
 
         // Return the expiration date
         return new Date(Long.parseLong(parts[1]));
+    }
+
+    public static boolean isValidToken(String token, SessionFactory sessionFactory) {
+        // Extract the user ID from the token
+
+        ClientDAO dao = new ClientDAO(sessionFactory);
+        // Get the client by ID
+        Client client = dao.getClientById( getUserId(token));
+
+        // Check if the client exists
+        if (client == null) {
+            return false;
+        }
+
+        // Check the expiration date
+        Date expirationDate = getExpirationDate(token);
+        Date currentDate = new Date();
+        if (expirationDate.before(currentDate)) {
+            return false;
+        }
+
+        return true;
     }
 }
