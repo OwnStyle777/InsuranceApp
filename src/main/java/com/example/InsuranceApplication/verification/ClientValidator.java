3df90cfd937086615e421826539ba50c7f2aa3de
OwnStyle777 <OwnStyleXD@gmail.com>
2024-01-27 14:51:45 +0100

    add Registration form as argument in validateLoginInfo and validateClient for hashing purposes

--- src/main/java/com/example/InsuranceApplication/verification/ClientValidator.java
+++ src/main/java/com/example/InsuranceApplication/verification/ClientValidator.java
@@ -3,19 +3,20 @@ package com.example.InsuranceApplication.verification;
 import com.example.InsuranceApplication.client.Client;
 import com.example.InsuranceApplication.client.LoginInfo;
 import com.example.InsuranceApplication.client.PersonalData;
+import com.example.InsuranceApplication.forms.RegistrationForm;
 import com.example.InsuranceApplication.insurance.Insurance;
 
 import java.util.Date;
 
 public interface ClientValidator extends PersonalDataValidation, InsuranceDataValidation, PasswordValidator, EmailValidator{
 
-    default  boolean validateClient(Client client){
+    default  boolean validateClient(Client client, RegistrationForm form){
 
         System.out.println("Validating client: " + client.toString());
         Insurance insurance = client.getInsuranceInfo();
         PersonalData personalData = client.getPersonalData();
         LoginInfo loginInfo = client.getLoginInfo();
-        return validateInsuranceData(insurance) && validatePersonalData(personalData) && validateLoginInfo(loginInfo);
+        return validateInsuranceData(insurance) && validatePersonalData(personalData) && validateLoginInfo(loginInfo, form);
     }
     default boolean validatePersonalData (PersonalData personalData){
         String firstName = personalData.getFirstName();
@@ -31,9 +32,9 @@ public interface ClientValidator extends PersonalDataValidation, InsuranceDataVa
 
         return isBirthNumberValid(birthNumber) && isValidInsuranceCompany(insuranceCompany);
     }
-    default boolean validateLoginInfo (LoginInfo loginInfo){
+    default boolean validateLoginInfo (LoginInfo loginInfo, RegistrationForm form){
         String email = loginInfo.getEmail();
-        String password = loginInfo.getPassword();
+        String password = form.getPassword();
 
         return validatePassword(password) && isEmailValid(email);
     }
