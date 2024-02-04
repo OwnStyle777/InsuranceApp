13f724dfc8a2062a8510220bc60d0684d7b580dc
OwnStyle777 <OwnStyleXD@gmail.com>
2024-02-04 17:58:34 +0100

    change endpoint clienInfo to dynamic endpoint for each client another endpoint with userID

--- src/main/java/com/example/InsuranceApplication/client/HtmlViewController.java
+++ src/main/java/com/example/InsuranceApplication/client/HtmlViewController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,8 +28,8 @@ public class HtmlViewController {
     public String showRegisterPage() {
         return "form";
     }
-    @GetMapping("/clientInfo")
-    public String showClientPage() {
+    @GetMapping("/clientInfo/{userId}")
+    public String showClientPage(@PathVariable Long userId) {
         return "client";
     }
     @GetMapping ("/hello" )
