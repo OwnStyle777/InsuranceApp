bdc6b2c1725012e7e20c9e3eb69e6c3f6eb81892
OwnStyle777 <OwnStyleXD@gmail.com>
2024-02-22 08:36:06 +0100

    add line which delete authToken

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -136,6 +136,8 @@ function deleteAllCookies() {
             };
             d.shift();
         }
+
+        document.cookie = 'authToken=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/Insurance';
         console.log(cookies);
     }
 
