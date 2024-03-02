ce64cb872c60f3ec73a0cad368995a29f5c9e2df
OwnStyle777 <OwnStyleXD@gmail.com>
2024-03-02 19:24:57 +0100

    create ClientProfilePicture entity

---
+++ src/main/java/com/example/InsuranceApplication/client/ClientProfilePicture.java
@@ -0,0 +1,31 @@
+package com.example.InsuranceApplication.client;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "user_profile_picture")
+public class ClientProfilePicture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(name = "image_data")
+    private byte[] imageData;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+}
