a6a52dc403e93250514dc60e2b8fb32f74e339f6
OwnStyle777 <OwnStyleXD@gmail.com>
2024-03-05 22:32:48 +0100

    add toString method

--- src/main/java/com/example/InsuranceApplication/client/ClientProfilePicture.java
+++ src/main/java/com/example/InsuranceApplication/client/ClientProfilePicture.java
@@ -2,14 +2,27 @@ package com.example.InsuranceApplication.client;
 
 import jakarta.persistence.*;
 
+import java.util.Arrays;
+
 @Entity
-@Table (name = "user_profile_picture")
+@Table(name = "user_profile_picture")
 public class ClientProfilePicture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
+    @Column(name = "image_id")
+    private Long imageId;
+
+    @Override
+    public String toString() {
+        return "ClientProfilePicture{" +
+                "id=" + id +
+                ", imageId=" + imageId +
+                ", imageData=" + Arrays.toString(imageData) +
+                '}';
+    }
+
     @Column(name = "image_data")
     private byte[] imageData;
 
@@ -19,6 +32,14 @@ public class ClientProfilePicture {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(Long imageId) {
+        this.imageId = imageId;
     }
 
     public byte[] getImageData() {
